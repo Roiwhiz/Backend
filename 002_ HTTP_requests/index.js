f9613b404107c9a3c38ff930aaca9132564b993d
Salaudeen Toheeb <roiwhiz@gmail.com>
2025-03-13 09:23:51 +0100

    Add http request with different route

--- 002_ HTTP_requests/index.js
+++ 002_ HTTP_requests/index.js
@@ -6,6 +6,10 @@ app.get("/", (req,res) => {
     res.send("Welcome!")
 })
 
+app.get("/about", (req,res) => {
+    res.send("My name is Salaudeen Toheeb. I am a fullstack web developer")
+})
+
 app.listen(port, () => {
     console.log(`Server running at port ${port}.`)
 })
